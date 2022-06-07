4bf69ed5e9bee50b354be8092fd84b73e0244341
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-06-07 16:20:12 +1200

    still only focusing on a single endpoint /resturants. The code in the endpoint is now wrapped in a try block, so that the code will be tested for run time errors when it is exectued. The try block is followed by a catch block which will handle any errors. I have written a shcema for Joi validation in RestaurantSchema.js, I dont full understand where Joi fits into the picture yet, that is something I am treying to learn during this project. I have not included any auth0 code yet as i dont want to have to deal with authorsing myself while developiong. I am going to write automated tests of all the files accosated with the get/resturants endpoint. For the express route, formatResturant ect.

---
+++ server/src/RestaurantSchema.js
@@ -0,0 +1,8 @@
+const Joi = require("joi");
+
+const schema = Joi.object().keys({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  image: Joi.string().required(),
+});
+module.exports = schema;
--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,35 @@
 const express = require("express");
 const cors = require("cors");
-const app = express();
+const { celebrate, Joi, errors, Segments } = require("celebrate");
+
 const restaurantModel = require("./models/RestaurantModel");
 const formatRestaurants = require("./formatRestaurants");
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/restaurants", async (request, response) => {
-  const restaurants = await restaurantModel.find({});
-  const formattedRestaurants = restaurants.map((restaurant) => {
-    return formatRestaurants(restaurant);
-  });
-  return response.status(200).send(formattedRestaurants);
-});
+app.get(
+  "/restaurants",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required(),
+    }),
+  }),
+  async (request, response, next) => {
+    try {
+      const restaurants = await restaurantModel.find({});
+      const formattedRestaurants = restaurants.map((restaurant) => {
+        return formatRestaurants(restaurant);
+      });
+      return response.status(200).send(formattedRestaurants);
+    } catch (error) {
+      error.status = 400;
+      next(error);
+    }
+  }
+);
 
 module.exports = app;
