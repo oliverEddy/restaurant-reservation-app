039f1e6030bb2c70294b6c84881c1e9a9d248f85
OLIVER EDDY <oliver.eddy@developersinstitute.co.nz>
2022-06-17 15:43:31 +1200

    deleted unsed CSS selectors and RestaurantSchema.js.

--- server/src/RestaurantSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require("joi");
-
-const schema = Joi.object().keys({
-  name: Joi.string().required(),
-  description: Joi.string().required(),
-  image: Joi.string().required(),
-});
-module.exports = schema;
